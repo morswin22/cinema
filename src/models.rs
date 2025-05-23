cca81c26db1a2246b5d7d2b4268eaaf7a9b8c2a8
Marek Seget <marek.seget1@gmail.com>
2025-05-23 16:13:25 +0200

    chore(models): update structs from database

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,13 @@
 use diesel::prelude::*;
 use chrono::NaiveDateTime;
 use crate::schema::*;
+use serde::{Deserialize, Serialize};
+use diesel::deserialize::QueryableByName; // Import QueryableByName
 
-#[derive(Queryable, Identifiable, Selectable)]
+// User Model
+#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
 #[diesel(table_name = users)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct User {
     pub id: i32,
     pub email: String,
@@ -12,13 +16,16 @@ pub struct User {
 
 #[derive(Insertable)]
 #[diesel(table_name = users)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct NewUser<'a> {
     pub email: &'a str,
     pub password: &'a str,
 }
 
-#[derive(Queryable, Identifiable, Selectable, Debug)]
+// Movie Model
+#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
 #[diesel(table_name = movies)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct Movie {
     pub id: i32,
     pub title: String,
@@ -28,14 +35,17 @@ pub struct Movie {
 
 #[derive(Insertable)]
 #[diesel(table_name = movies)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct NewMovie<'a> {
     pub title: &'a str,
     pub year: i32,
     pub director: &'a str,
 }
 
-#[derive(Queryable, Identifiable, Selectable)]
+// Room Model
+#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
 #[diesel(table_name = rooms)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct Room {
     pub id: i32,
     pub capacity: i32,
@@ -44,13 +54,16 @@ pub struct Room {
 
 #[derive(Insertable)]
 #[diesel(table_name = rooms)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct NewRoom<'a> {
     pub capacity: i32,
     pub label: &'a str,
 }
 
-#[derive(Queryable, Identifiable, Selectable)]
+// Schedule Model
+#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
 #[diesel(table_name = schedule)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct Schedule {
     pub id: i32,
     pub movie_id: i32,
@@ -60,16 +73,19 @@ pub struct Schedule {
 
 #[derive(Insertable)]
 #[diesel(table_name = schedule)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct NewSchedule {
     pub movie_id: i32,
     pub room_id: i32,
     pub date: NaiveDateTime,
 }
 
-#[derive(Queryable, Identifiable, Associations, Selectable)]
+// Reservation Model
+#[derive(Queryable, Identifiable, Associations, Selectable, Debug, PartialEq)]
 #[diesel(belongs_to(User))]
 #[diesel(belongs_to(Schedule))]
 #[diesel(table_name = reservation)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct Reservation {
     pub id: i32,
     pub user_id: i32,
@@ -78,7 +94,40 @@ pub struct Reservation {
 
 #[derive(Insertable)]
 #[diesel(table_name = reservation)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
 pub struct NewReservation {
     pub user_id: i32,
     pub schedule_id: i32,
+}
+
+// New struct for updating Reservation
+#[derive(AsChangeset, Deserialize, Serialize, Debug)]
+#[diesel(table_name = reservation)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
+pub struct ReservationChangeset {
+    pub user_id: Option<i32>,
+    pub schedule_id: Option<i32>,
+}
+
+// Struct for displaying reservation details (joins)
+// Changed to QueryableByName for raw SQL queries
+#[derive(Debug, QueryableByName)]
+pub struct ReservationDetail {
+    #[diesel(sql_type = diesel::sql_types::Integer)]
+    pub reservation_id: i32,
+    #[diesel(sql_type = diesel::sql_types::Varchar)]
+    pub user_email: String,
+    #[diesel(sql_type = diesel::sql_types::Varchar)]
+    pub movie_title: String,
+    #[diesel(sql_type = diesel::sql_types::Varchar)]
+    pub room_label: String,
+    #[diesel(sql_type = diesel::sql_types::Timestamp)] // Use Timestamp for NaiveDateTime
+    pub schedule_date: NaiveDateTime,
+}
+
+// New struct to handle LAST_INSERT_ID() for MySQL
+#[derive(Debug, QueryableByName)]
+pub struct LastInsertId {
+    #[diesel(sql_type = diesel::sql_types::Bigint)] // Specify the SQL type
+    pub id: i64, // Use i64 as LAST_INSERT_ID() returns BIGINT
 }
